7806bf56ea6c4c07442269ed520f0246c1b3c6bf
erybak90 <wcia@radiowy.net>
2017-04-29 22:15:22 +0200

    Session helper next

--- test_contact/test_add_contact.py
+++ test_contact/test_add_contact.py
@@ -14,21 +14,21 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_add_new_contact_form()
     app.init_fill_contact_form(
         Contact(Firstname="First name", Lastname="Last name", Nickname="NIckname", company="Corpo", adress="Poland",
                 mobile="698998657",
                 email="wcia@radiowy.net", notes="Dodanie kontaktu"))
     app.submit_contact_creation()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_add_new_contact_form()
     app.init_fill_contact_form(Contact(Firstname="", Lastname="", Nickname="", company="", adress="", mobile="",
                                        email="", notes=""))
     app.submit_contact_creation()
-    app.logout()
+    app.session.logout()
