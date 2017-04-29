05c68ac74d36ffecec99f30b9671fb8f56a83818
erybak90 <wcia@radiowy.net>
2017-04-29 18:29:06 +0200

    Class for work with groups

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,10 +13,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Nowe 1 zadanie", header="Nowe zadanie", footer="Nowe zadanie"))
+    app.group.create(Group(name="Nowe 1 zadanie", header="Nowe zadanie", footer="Nowe zadanie"))
     app.session.logout()
 
 def test_add__empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
