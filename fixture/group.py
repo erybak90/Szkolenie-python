31d10ec699b3f832cc64302f4ca09a8479937071
erybak90 <wcia@radiowy.net>
2017-05-01 22:16:08 +0200

    skype_01.05

--- fixture/group.py
+++ fixture/group.py
@@ -3,6 +3,24 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    def edit_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("abc")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("def")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("ghi")
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("group page").click()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
--- test_contact/delete_first_contact.py
+++
@@ -1,6 +0,0 @@
-
-def test_add_contact(app):
-    app.open_home_page()
-    app.session.login(username="admin", password="secret")
-    app.contact.delete_first_contact()
-    app.session.logout()
