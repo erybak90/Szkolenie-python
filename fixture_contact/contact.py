34d633fdca0d30394c96194e3f0647d7419920ab
erybak90 <wcia@radiowy.net>
2017-05-01 18:17:45 +0200

    propably works

--- fixture_contact/contact.py
+++ fixture_contact/contact.py
@@ -4,6 +4,13 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
 
     def submit_contact_creation(self):
         wd = self.app.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,6 +3,8 @@ from model.group import Group
 
 
 
+
+
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Nowe 1 zadanie", header="Nowe zadanie", footer="Nowe zadanie"))
