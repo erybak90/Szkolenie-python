ffb74199cdf55692fbec2a3f9483ce72ac06b5d6
erybak90 <wcia@radiowy.net>
2017-05-03 21:07:51 +0200

    intellectual fixture

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,11 +4,7 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Nowe 1 zadanie", header="Nowe zadanie", footer="Nowe zadanie"))
-    app.session.logout()
 
 def test_add__empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
