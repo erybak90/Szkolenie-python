c71895fb9ef0299d0474fd7ffcda3c610f202ffc
erybak90 <wcia@radiowy.net>
2017-05-03 22:30:23 +0200

    problem_intelligence_login

--- fixture/session.py
+++ fixture/session.py
@@ -3,6 +3,30 @@ class SessionHelper:
     def __init__(self, app):
         self.app = app
 
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
+
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_element_by_link_text("Logout")) > 0
 
     def login(self, username, password):
         wd = self.app.wd
@@ -19,3 +43,4 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="Nowe 1 zadanie", header="Nowe zadanie", footer="Nowe zadanie"))
+    app.session.logout()
 
 def test_add__empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
