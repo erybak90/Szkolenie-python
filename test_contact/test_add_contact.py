6505af3e84e6e2091c8b9f7af3d7e13542aad3f3
erybak90 <wcia@radiowy.net>
2017-05-06 19:09:17 +0200

    Task 6

--- test_contact/test_add_contact.py
+++ test_contact/test_add_contact.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from model_contact.contact import Contact
+from model.contact import Contact
 
 
 def test_add_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.group.open_add_new_contact_form()
-    app.group.init_fill_contact_form(
+    app.contact.open_add_new_contact_form()
+    app.contact.init_fill_contact_form(
         Contact(Firstname="First name", Lastname="Last name", Nickname="NIckname", company="Corpo", adress="Poland",
                 mobile="698998657",
                 email="wcia@radiowy.net", notes="Dodanie kontaktu"))
-    app.group.submit_contact_creation()
+    app.contact.submit_contact_creation()
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.group.open_add_new_contact_form()
-    app.group.init_fill_contact_form(Contact(Firstname="", Lastname="", Nickname="", company="", adress="", mobile="",
+    app.contact.open_add_new_contact_form()
+    app.contact.init_fill_contact_form(Contact(Firstname="", Lastname="", Nickname="", company="", adress="", mobile="",
                                        email="", notes=""))
-    app.group.submit_contact_creation()
+    app.contact.submit_contact_creation()
     app.session.logout()
--- test_contact/test_del_contact.py
+++ test_contact/test_del_contact.py
@@ -2,5 +2,5 @@
 def test_delete_first_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.group.delete_first_contact()
+    app.contact.delete_first_contact()
     app.session.logout()
