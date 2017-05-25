1aee636ccc886c83a2396fafbff916b6971beef6
erybak90 <wcia@radiowy.net>
2017-05-25 21:57:55 +0200

    25.05 works

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,13 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="Nowe 1 zadanie", header="Nowe zadanie", footer="Nowe zadanie"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add__empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
