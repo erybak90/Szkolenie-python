cf8bad0ecd6840d34b803b294b32e6442f48c842
erybak90 <wcia@radiowy.net>
2017-06-10 22:26:24 +0200

    Task No 13

--- fixture/contact.py
+++ fixture/contact.py
@@ -4,9 +4,9 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def modify_first_contact(self, new_contact_data):
+    def modify_contact_by_index(self, index, new_contact_data):
         wd = self.app.wd
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         # open modification form
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
         # fill contact form
@@ -16,14 +16,25 @@ class ContactHelper:
         wd.find_element_by_link_text("home").click()
         self.contact_cache = None
 
+    def modify_first_contact(self):
+        self.modify_contact_by_index(0)
+
+
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         # submit deletion
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
         wd.find_element_by_link_text("home").click()
         self.contact_cache = None
+
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def select_first_contact(self):
         wd = self.app.wd
--- test_contact/test_edit_first_contact.py
+++ test_contact/test_edit_first_contact.py
@@ -1,11 +1,18 @@
 from model.contact import Contact
+from random import randrange
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact(Contact(Firstname="First name", Lastname="Last name", Nickname="NIckname", company="Corpo", address="Poland",
+                mobile="698998657",
+                email="wcia@radiowy.net", notes="Dodanie kontaktu"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(Firstname="New firstname"))
+    index = randrange (len(old_contacts))
+    app.contact.modify_contact_by_index(index, Contact(Firstname="New firstname"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+
 
 
 #def test_modify_contact_nickname(app):
