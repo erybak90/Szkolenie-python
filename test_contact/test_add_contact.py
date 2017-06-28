b674ad9c3ce143e854b12976f3dba0d1803bb20e
erybak90 <wcia@radiowy.net>
2017-06-28 19:33:31 +0200

    Task 11 New

--- test_contact/test_add_contact.py
+++ test_contact/test_add_contact.py
@@ -1,23 +1,17 @@
-from model.contact import Contact
 
-def test_add_contact(app):
+from model.contact import Contact
+import pytest
+import random
+import string
+from data.add_contact import constant as testdata
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+    pass
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(Firstname="Firstname", Lastname="Lastname", address="Address",
-                      homephone="000000000", mobilephone="698998657",
-                workphone="555588888", secondaryphone="888889999", email="xyz@onet.pl", email2="abc@abc.pl", email3="jkl@ok.pl")
     app.contact.open_add_new_contact_form()
     app.contact.init_fill_contact_form(contact)
     app.contact.submit_contact_creation()
-    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-
-
-#def test_add_empty_contact(app):
-#    old_contacts = app.contact.get_contact_list()
-#    contact = Contact(Firstname="", Lastname="", Nickname="", company="", address="", mobile="", email="", notes="")
-#    app.contact.open_add_new_contact_form()
-#    app.contact.init_fill_contact_form(contact)
-#    app.contact.submit_contact_creation()
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
-
+    assert len(old_contacts) + 1 == len(new_contacts)
+    sorted(old_contacts + [contact], key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test_contact/test_edit_first_contact.py
+++ test_contact/test_edit_first_contact.py
@@ -4,15 +4,17 @@ from random import randrange
 
 def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
-        app.contact(Contact(Firstname="First name", Lastname="Last name", Nickname="NIckname", company="Corpo",
+        app.contact(Contact(Firstname="First name", Lastname="Last name", homephone="NIckname", mobilephone="Corpo",
                             address="Poland",
-                            mobile="698998657",
-                            email="wcia@radiowy.net", notes="Dodanie kontaktu"))
+                            workphone="698998657",
+                            email="wcia@radiowy.net", email2="Dodanie kontaktu"))
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
     app.contact.modify_contact_by_index(index, Contact(Firstname="New firstname"))
     new_contacts = app.contact.get_contact_list()
+    old_contacts[0] = old_contacts
     assert len(old_contacts) == len(new_contacts)
+    assert old_contacts == new_contacts
 
 
 #def test_modify_contact_nickname(app):
